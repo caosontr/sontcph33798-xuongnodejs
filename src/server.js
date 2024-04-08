c18553b745e003c7b9d30d0449213e63baa285aa
caosontr <caosontr@gmail.com>
2024-04-08 20:06:05 +0700

    first commit

---
+++ src/server.js
@@ -0,0 +1,26 @@
+import express from "express";
+import connectDB from "./config/dbconfig.js";
+import movieRouter from "./routes/movie.js";
+import categoryRouter from "./routes/category.js";
+import genreRouter from "./routes/genre.js";
+import userRouter from "./routes/user.js";
+import dotenv from "dotenv";
+const app = express();
+dotenv.config();
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+);
+app.use(express.json());
+app.use(movieRouter);
+app.use(categoryRouter);
+app.use(genreRouter);
+app.use(userRouter);
+connectDB(process.env.DB_URI);
+
+const port = process.env.PORT;
+
+app.listen(port, () => {
+  console.log("Server is running on port 3000");
+});
